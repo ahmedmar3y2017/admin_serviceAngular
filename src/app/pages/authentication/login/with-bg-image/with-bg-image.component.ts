e8635aa21fea9e8440c76e8526f58b3751a89ce7
AshrafZidan <ashrafzidan996@gmail.com>
2018-06-11 20:30:37 -0700

    Add files via upload

--- src/app/pages/authentication/login/with-bg-image/with-bg-image.component.ts
+++ src/app/pages/authentication/login/with-bg-image/with-bg-image.component.ts
@@ -1,4 +1,10 @@
+import { UserService } from './../../../../shared/Services/user.service';
+import { element } from 'protractor';
+import { Router } from '@angular/router';
+import { AuthComponent } from './../../../../layout/auth/auth.component';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup , Validators  } from '@angular/forms';
+
 
 @Component({
   selector: 'app-with-bg-image',
@@ -6,10 +12,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./with-bg-image.component.css']
 })
 export class WithBgImageComponent implements OnInit {
+  IsAuth:boolean;
 
-  constructor() { }
+
+  complexForm : FormGroup;
+
+
+  constructor(private loginService : UserService , private loginRouter:Router , fb: FormBuilder) {
+
+    this.complexForm = fb.group({
+
+      'email' :  [null, Validators.compose([Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')])],
+
+      'password': [null, Validators.required],
+
+    })
+  }
 
   ngOnInit() {
+
   }
+
+  loginFun(e){
+    e.preventDefault();
+    var user = e.target.elements[0].value;
+    var pass = e.target.elements[1].value;
+
+
+    console.log(user);
+    if (user =="admin@gmail.com" && pass == "admin") {
+    this.loginService.SetUserLoggedIn();
+    this.loginRouter.navigate(['dashboard']);
+    }
+     else {
+    this.loginRouter.navigate(['authentication/login']);
+    }
+}
 
 }
